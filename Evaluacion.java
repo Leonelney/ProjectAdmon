a1bf6473b0fa84c22442d2de9bc77d4a71e26526
Carlos Armando Ochoa Ginera <caog.freeman@gmail.com>
2017-11-30 22:37:35 -0600

    Cambiados un par de parametros y realizados casts necesarios

--- Evaluacion.java
+++ Evaluacion.java
@@ -2,7 +2,7 @@ package Sources;
 
 //clase para realizar la operaciones de un año en especifico.
 public class Evaluaciones {
-    int uniVendidas = 20000;      //unidades estimadas a vender(iniciales)
+    private int uniVendidas = 20000;      //unidades estimadas a vender(iniciales)
     int uniVendidasA;           // unidades vendidas con aumento por año
     double porRed;              //
     double ventasPre;           //ventas previstas
@@ -21,12 +21,12 @@ public class Evaluaciones {
 
     public int obtenerUnidadesVen(int año){
     //calcula las unidades que se venden dependiendo del año
-        uniVendidasA = uniVendidas*Math.pow(1+.05,año-2017);
-        return unidadesVendidasA;
+        uniVendidasA = uniVendidas * (int) Math.pow(1 + .05, año - 2017);
+        return uniVendidasA;
     }
     public double obtenerVentasNetas(int año) {
         //calcula las ventas previstas
-        uniVendidas=obtenerUnidadesVen();//ajustando las unidades vendidas respectivas al año
+        uniVendidas=obtenerUnidadesVen(año);//ajustando las unidades vendidas respectivas al año
         ventasPre = (uniVendidas * precio) * Math.pow( 1 + 0.05 , año-2017 );
         ventasNetas = ventasPre - (ventasPre / (1 + iva)) * iva;
         return ventasNetas;
@@ -34,7 +34,7 @@ public class Evaluaciones {
 
     public double obtenerCostosProd(int año) {
         //calcula los costos de produccion
-        uniVendidas=obtenerUnidadesVen();//ajustando las unidades vendidas respectivas al año
+        uniVendidas = obtenerUnidadesVen(año);//ajustando las unidades vendidas respectivas al año
         costosProd = ((((120 + 80 + 50 + 50) * uniVendidas) + 150000) * Math.pow( 1 + 0.08 , año-2017 )) + 100000 + 200000;
         return costosProd;
     }
@@ -52,12 +52,12 @@ public class Evaluaciones {
 
     public double obtenerUtilidadBruta(int año) {
         //se calcula la utilidad bruta restandole a las ventas prevista los costos de produccion y la depreciacion
-        utilidadbruta = obtenerVentasNetas() - obtenerCostosProd() - obtenerDepreciacion();
+        utilidadbruta = obtenerVentasNetas(año) - obtenerCostosProd(año) - obtenerDepreciacion(año);
         return utilidadbruta;
     }
 
     public double obtenerGastosAdmon(int año) {
-        uniVendidas=obtenerUnidadesVen();//ajustando las unidades vendidas respectivas al año
+        uniVendidas=obtenerUnidadesVen(año);//ajustando las unidades vendidas respectivas al año
         //calculo de los gastos de administracion
         gastosAdmon = (100 * uniVendidas) * Math.pow( 1 + 0.05 , año-2017 );
         return gastosAdmon;
@@ -65,19 +65,19 @@ public class Evaluaciones {
 
     public double obtenerGastosVentas(int año) {
         //calculo de los gastos de ventas
-        uniVendidas=obtenerUnidadesVen();//ajustando las unidades vendidas respectivas al año
+        uniVendidas=obtenerUnidadesVen(año);//ajustando las unidades vendidas respectivas al año
         gastosVentas = (200 * uniVendidas) * Math.pow( 1 + 0.05 , año-2017 );
         return gastosVentas;
     }
 
-    public double obtenerUtilidadOper() {
+    public double obtenerUtilidadOper(int año) {
         //calcula la utilidad de operacion restandole a la utilidad bruta los gastos de administracion y ventas
-        utilidadOper = obtenerUtilidadBruta() - obtenerGastosAdmon() - obtenerGastosVentas();
+        utilidadOper = obtenerUtilidadBruta(año) - obtenerGastosAdmon(año) - obtenerGastosVentas(año);
         return utilidadOper;
     }
 
-    public double obtenerImpuestos() {
-        utilidadOper = obtenerUtilidadOper();
+    public double obtenerImpuestos(int año) {
+        utilidadOper = obtenerUtilidadOper(año);
         //dependiendo del resultado de la utilidad de operacion se calcula el impuesto
         if (utilidadOper > 0 && utilidadOper <= 999) {
             impuestos = utilidadOper * 0.05;
@@ -98,9 +98,9 @@ public class Evaluaciones {
     }
 
     //metodo que calcula el valor de la utilidad del ejercicio en el año en especifico.
-    public double obtenerUtilidad() {
+    public double obtenerUtilidad(int año) {
         //calcula la utilidad del ejercicio restandole a la utilidad de operacion los impuestos 
-        utilidad = obtenerUtilidadOper() - obtenerImpuestos();
+        utilidad = obtenerUtilidadOper(año) - obtenerImpuestos(año);
         return utilidad;
     }
 }
